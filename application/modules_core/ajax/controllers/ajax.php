3a21c026a90918e578a1354da84dcc8ce4ec09f8
Damiano Venturin <dam@venturin.net>
2012-10-19 20:56:24 +0200

    fixed a little bug

--- application/modules_core/ajax/controllers/ajax.php
+++ application/modules_core/ajax/controllers/ajax.php
@@ -689,10 +689,10 @@ class Ajax extends Admin_Controller {
     	if($oid = $this->mcbsb->tooljar->get_my_tj_organization()) {
     		$to_js['status'] = true;
     		$to_js['oid'] = $oid;
-    		$this->output($to_js);
     	} else {
     		//TODO if I want to bother the user because of the fact he didn't set up a TJ company yet, this is the place to add code
     		$to_js['status'] = false;
     	}
+    	$this->output($to_js);
     }    
 }
