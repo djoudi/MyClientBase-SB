64ab8985b84b9e53c98b88e3ef8e2f8c02f57303
Damiano Venturin <dam@venturin.net>
2012-10-20 22:43:23 +0200

    added alter to enable ToolJar module

--- application/config/mcbsb.php
+++ application/config/mcbsb.php
@@ -1,4 +1,4 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['mcbsb_version'] = '0.13.23';
+$config['mcbsb_version'] = '0.13.24';
 $config['validate_url'] = true;
