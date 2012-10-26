aa84d61bd634d1098ec0f4131b95da5eecb3542a
Damiano Venturin <dam@venturin.net>
2012-10-27 01:57:58 +0200

    #117

--- application/modules/tooljar/config/tooljar.php
+++ application/modules/tooljar/config/tooljar.php
@@ -1,4 +1,4 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['tooljar_server'] = "http://domagictooljar/api";
-$config['tooljar_ce_key'] = 'CcaEy2YUWQeoGDsZ4tMzFlJ7LxvXj12345K8nNcv9HAlVLtiOQau1MT0YhBmCjPz';
+$config['tooljar_ce_key'] = '';
