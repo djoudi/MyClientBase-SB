2859e5ae196243b4c8c025c609452f6bb1b502be
Damiano Venturin <dam@venturin.net>
2012-11-08 02:13:25 +0100

    changed version, added gitsubmodules, modified alter_file

--- application/config/mcbsb.php
+++ application/config/mcbsb.php
@@ -1,4 +1,4 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['mcbsb_version'] = '0.13.24';
+$config['mcbsb_version'] = '0.13.31';
 $config['validate_url'] = true;
