98bc9f43fc10cd816a5818d655071cb0e6aa478f
Damiano Venturin <dam@venturin.net>
2012-12-24 04:17:24 +0100

    back to previous configuration. the bug was in tooljar

--- application/modules/tooljar/models/mdl_tooljar.php
+++ application/modules/tooljar/models/mdl_tooljar.php
@@ -80,7 +80,9 @@ class Mdl_Tooljar extends MY_Model {
 		$this->rest->initialize(array('server' => $this->tooljar_server));
 		$method = 'get_tj_admin_email';
 		$input = array();
-		if($this->config->item('tooljar_ce_key')) {
+		
+		if($this->config->item('ce_key')) {
+			//ce_key contained in rest.php config file
 			$input['ce_key'] = $this->config->item('ce_key');
 		} else {
 			return false;
